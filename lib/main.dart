dc3fb66eeb3ae9134d664ca1fab8f116137208fa
Dentsor <dentsor@dentsor.net>
2020-01-04 00:24:11 +0100

    Completing the video course from Lets Build That App: youtu.be/S59b-XFsyY8

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+import './views/video_cell.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(new RealWorldApp());
+
+class RealWorldApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RealWorldState();
+  }
+}
+
+class RealWorldState extends State<RealWorldApp> {
+  var _isLoading = true;
+
+  var videos;
+
+  _fetchData() async {
+    print("Attempting to fetch data from network");
+
+    final url = "http://api.letsbuildthatapp.com/youtube/home_feed";
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // print(response.body);
+
+      final map = json.decode(response.body);
+      final videosJson = map["videos"];
+      /*videosJson.forEach((video) {
+        print(video["name"]);
+      });*/
+
+      setState(() {
+        _isLoading = false;
+        this.videos = videosJson;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("REAL WORLD APP BAR"),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.refresh),
+              onPressed: () {
+                print("Reloading...");
+                setState(() {
+                  _isLoading = true;
+                });
+                _fetchData();
+              })
+        ],
+      ),
+      body: new Center(
+        child: _isLoading
+            ? new CircularProgressIndicator()
+            : new ListView.builder(
+                itemCount: this.videos != null ? this.videos.length : 0,
+                itemBuilder: (context, i) {
+                  final video = this.videos[i];
+
+                  return new FlatButton(
+                      padding: new EdgeInsets.all(0.0),
+                      child: new VideoCell(video),
+                      onPressed: () {
+                        print("Video cell tapped: $i");
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) =>
+                                    new DetailPage()));
+                      });
+                  //return new VideoCell(video);
+                  //return new Text("Row: $i");
+                }),
+        //new Text("Finished Loading..."),
+      ),
+    ));
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final url = "http://api.letsbuildthatapp.com/youtube/course_detail?id=1";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Detail page"),
+      ),
+      body: new Center(
+        child: new Text("Detail detail detail"),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+/* class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -109,3 +214,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+ */
